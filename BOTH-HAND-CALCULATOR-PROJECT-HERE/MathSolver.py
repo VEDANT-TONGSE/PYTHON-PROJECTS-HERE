489558b56468035c25de056e493e700dad894fca
VEDANT ASHOK TONGSE <vedant.tongse.vt@gmail.com>
2026-02-16 23:56:00 +0530

    Update MathSolver.py

--- BOTH-HAND-CALCULATOR-PROJECT-HERE/MathSolver.py
+++ BOTH-HAND-CALCULATOR-PROJECT-HERE/MathSolver.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
   
 mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+mp_hands = mp.solutions.hands  
 hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7,
     min_tracking_confidence=0.7)
 
